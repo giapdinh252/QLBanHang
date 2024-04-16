d9c60bd2151f604c07914cf65f427235844029e3
giapdinh252 <119155784+giapdinh252@users.noreply.github.com>
2024-04-16 10:30:08 +0700

    Update HoaDonBan.java

--- src/com/mycompany/qlbh/model/HoaDonBan.java
+++ src/com/mycompany/qlbh/model/HoaDonBan.java
@@ -75,9 +75,7 @@ public class HoaDonBan {
         this.TongTien = TongTien;
     }
 
-    public void setNhanvien(String selectedValue) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-    // test commit
+    
+ 
     
 }
